0766ca6b38e61c752571fe9d0233ffaf28ebc1e7
Vivek Goyal <rcivivek41214008@gmail.com>
2023-07-08 19:31:36 +0530

    Add new Goal adding option

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import GoalList from './components/GoalList/GoalList.js';
 import NewGoal from './components/NewGoal/NewGoal.js';
-import './App.css'
+import './App.css';
+
+const courseGoals = [{id:'1', text:'Finish the Course'},
+{id:'2', text:'Learn all about Course Main Topic'},
+{id:'3', text:'Help other studends in the Course Q&A'}];
+
+
+
 const App=() =>{
-  const courseGoals = [{id:'1', text:'Finish the Course'},
+  const[courseGoals, setCourseGoals] = useState([{id:'1', text:'Finish the Course'},
   {id:'2', text:'Learn all about Course Main Topic'},
-  {id:'3', text:'Help other studends in the Course Q&A'}];
+  {id:'3', text:'Help other studends in the Course Q&A'}]);
+
+  const addNewGoalhandler =(newGoal) =>{
+    setCourseGoals(courseGoals.concat(newGoal))
+  };
   return  <div className='course-goal'> 
   <h2>Course Goals</h2>
-  <NewGoal/>
+  <NewGoal onAddGoal ={addNewGoalhandler}/>
    <GoalList goals={courseGoals}/>
 </div>
 };
--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './NewGoal.css';
-const NewGoal = ()=>{
-    const addGoalhandler =event=>{
-        event.preventDefault();        
+const NewGoal = props=>{
+    const addGoalhandler = event => {
+        event.preventDefault();
+        const  newGoal = {
+             id  : Math.random().toString(),
+            text : 'My new Goal'
+        };
+        props.onAddGoal(newGoal);
     };
     return <form className='new-goal' onSubmit={addGoalhandler}>
         <input  type="text"/>
